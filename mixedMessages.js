bf34219a2230cb5f077ca711718856e771ff7ea1
Ian Hartwell <ianhartwell86@gmail.com>
2025-05-27 22:16:49 +0100

    Add mixedMessages.js

---
+++ mixedMessages.js
@@ -0,0 +1,12 @@
+// Task - Create a message generator using JavaScript. Develop locally and utilise Git version control via the command line.
+/* Plan:
+    - Create a random message generator
+    - Utilise random number generators e.g. Math.floor(Math.random() * array.length)
+    - Create a message output that has a number of replaceable elements
+        - E.g. `${Greeting}! Today could be ${descriptor}. ${Outro}.`
+                "Hey there! Today could be your lucky day. You got this!"
+    - Create a number of different arrays [Greeting, Descriptor, Outro]
+    - Use the random number generator to generate a random index for each array to construct a random message.
+    - Output the final message so that it is randomised each time */
+
+    
